4083d9042003679fbf703fdc6093631e6333b417
CashDeficiency <cashdeficiency@gmail.com>
2024-08-22 21:12:36 -0400

    Enable camera on host macbook

--- macbook/hardware-configuration.nix
+++ macbook/hardware-configuration.nix
@@ -29,5 +29,8 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.config.allowUnfree = true;
+
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.facetimehd.enable = true;
 }
